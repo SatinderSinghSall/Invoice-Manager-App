2bd3498ead11b72286640b41ea48341b5b00ba00
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-02 13:44:24 +0530

    App first commit!

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Invoice Manager App",
+  description:
+    "Full-Stack NextJS App - Invoice Manager App -> CRUD, Server Actions, Search, Pagination & Send Emails.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center min-h-[82vh]">
+      <h2>Developer - Satinder Singh Sall.</h2>
+    </div>
+  );
+}
